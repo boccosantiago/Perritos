d3cc6254016c6ef59028228c63513e134f2aa1e6
Laura-Ruiz <laaura.26@gmail.com>
2022-09-01 16:24:01 +0200

    first commit

---
+++ src/datos.js
@@ -0,0 +1,74 @@
+const datos = [
+    {
+      shelterName: "",
+      email: "",
+      address: "",
+      phone: "",
+      pets: [
+        {
+          id: 1,
+          name: "paco",
+          age: "cachorro",
+          gender: "macho",
+          size: "pequeño",
+          breed: "chihuahua",
+          color: "blanco",
+          pelaje: "corto",
+          img: "./componetes/asd/img1",
+        },
+      ],
+    },
+    {
+      shelterName: "",
+      email: "",
+      address: "",
+      phone: "",
+      pets: [
+        {
+          name: "paco",
+          age: "cachorro",
+          gender: "macho",
+          size: "pequeño",
+          breed: "chihuahua",
+          color: "blanco",
+          pelaje: "corto",
+        },
+      ],
+    },
+    {
+      shelterName: "",
+      email: "",
+      address: "",
+      phone: "",
+      pets: [
+        {
+          name: "paco",
+          age: "cachorro",
+          gender: "macho",
+          size: "pequeño",
+          breed: "chihuahua",
+          color: "blanco",
+          pelaje: "corto",
+        },
+      ],
+    },
+    {
+      shelterName: "",
+      email: "",
+      address: "",
+      phone: "",
+      pets: [
+        {
+          name: "paco",
+          age: "cachorro",
+          gender: "macho",
+          size: "pequeño",
+          breed: "chihuahua",
+          color: "blanco",
+          pelaje: "corto",
+        },
+      ],
+    },
+  ];
+  
+  export default datos;
