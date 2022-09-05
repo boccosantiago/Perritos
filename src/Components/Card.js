0557b2305e8dca93b26090236fd4aa600da303d6
Laura-Ruiz <laaura.26@gmail.com>
2022-09-05 10:47:35 +0200

    Added css styles

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../datos"
 import "../Components/Card.css"
-function Card () {
-
+function Card (props) {
+    var value = props.filterData
     const infoProtect = data.map(item => item.pets)
+    const [petFilter, setPetFilter] = useState([...infoProtect])
     
-    const name = infoProtect.map(pets => pets.map(pet => pet.name));
-    const images = infoProtect.map(pets => pets.map(pet => console.log(`../img/id${pet.id}.jpg`)))
-    console.log(images)
+    const [petFound, setPetFound] = useState([...petFilter])
+
+
+    function filterPets() {
+        
+        
+        console.log(value, petFilter)
+        if (Object.values(value).length === 0) {
+            setPetFound([...petFilter])
+        } else {
+            console.log("hola")
+        }
+        
+        //else {
+        //     const filteredName = [...petFilter].filter(item => {
+        //         return item.name.toLowerCase().includes(lowerCaseValue)
+        //     });
+        //     setPetFound(filteredName);
+        //  }
+    }
+
+
+    
+    
+    // const name = infoProtect.map(pets => pets.map(pet => pet.name));
+    // const images = infoProtect.map(pets => pets.map(pet => console.log(`../img/id${pet.id}.jpg`)))
+    
     
     const cargarImagen = require.context("../img", true);
     
     return (
         <div id="container-card">
-            {infoProtect.map((pets, index) => (
+            <button onClick={filterPets}>Buscar</button>
+            {petFilter.map((pets, index) => (
                 pets.map(pet => (
                     <div key={`pet.id${pet.id}`} id="card">
                         <div id="container-img">
